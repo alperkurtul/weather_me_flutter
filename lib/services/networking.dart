89089d1fbc78e1d20ff743fa458f153d84c7142c
alperkurtul <alperkurtul@gmail.com>
2023-01-13 13:55:53 +0300

    improvements about api call requests

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -10,13 +12,32 @@ class NetworkHelper {
     Map retMap = Map();
 
     http.Response response;
-    try {
-      print('http.get TRY : ' + url.toString());
-      response = await http.get(url);
-      print('http.get SUCCESS : ' + url.toString());
-    } catch (e) {
-      print('http.get ERROR : ' + url.toString());
-      print('http.get ERROR : ' + e.message);
+
+    int triedCount = 0;
+    int maxTryCount = 5;
+    if (url.toString().contains('/2.5/find?')) {
+      maxTryCount = 1;
+    }
+    print('maxTryCount : ' + maxTryCount.toString());
+    while (response == null && triedCount < maxTryCount) {
+      triedCount++;
+      print('triedCount : ' + triedCount.toString());
+      try {
+        print('http.get TRY : ' + url.toString());
+        response = await http.get(url).timeout(Duration(seconds: 1+triedCount));
+        print('http.get SUCCESS : ' + url.toString());
+      } on TimeoutException catch(e) {
+        print('http.get ERROR TimeoutException : ' + url.toString());
+        print('http.get ERROR TimeoutException : ' + e.message);
+        //return retMap;
+      } catch (e) {
+        print('http.get ERROR : ' + url.toString());
+        print('http.get ERROR : ' + e.message);
+        //return retMap;
+      }
+    }
+
+    if (response == null) {
       return retMap;
     }
 
